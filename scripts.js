3212669d0ee3c9a38c69ecac05306780e4f90a92
Richard Radcliffe <radcliffer94@gmail.com>
2022-09-18 15:22:01 -0700

    Define output array and define game round function
    Define an output array to return both the console log
    verbage and the value to determine who won the round
    
    Define a game round function that plays the game five times
    and keeps track of score. Once game is over, print results of who won.

--- scripts.js
+++ scripts.js
@@ -1,62 +1,55 @@
-// define rock paper sizzors in an array
-let choices = ["rock", "paper", "scissors"]
 //define inital scores
 let computerScore = 0
 let playerScore = 0
-
-
 // get the computers random choice and return it
-function getComputerChoice(choices) {
+function getComputerChoice() {
+    // define rock paper sizzors in an array local to the function
+    let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-// define computer selection variable
-let computerSelection =  getComputerChoice(choices)
-console.log(computerSelection)
-
 // get the users input with an input function and return it 
 function getUserChoice () {
     return window.prompt("Enter your choice: ");
 }
-//define user selection variable 
-let playerSelection = "scissors"
-console.log(playerSelection)
-// = getUserChoice()
-
-
 // use logic to determine who is the winner in the match
-function determineWinner (p, c) {
+function determineWinner () {
+    // call the choice functions
+    p = getUserChoice().toLowerCase();
+    c = getComputerChoice();
     if (p === c) {
-        console.log("Tie!")
+        return winStr = ["Tie!", 0]
     }else if (p === "scissors" && c === "paper") {
-        console.log("You win! Scissors beats paper!")
+        return ["You win! Scissors beats paper!", "p"]
     }else if (p === "rock" && c === "scissors"){
-        console.log("You win! Rock beats scissors!")
+        return ["You win! Rock beats scissors!", "p"]
     }else if (p === "paper" && c === "rock"){
-        console.log("You win! Paper beats rock!")
+        return ["You win! Paper beats rock!", "p"]
     }else if (c === "rock" && p === "scissors"){
-        console.log("Computer wins! Rock beats scissors!")
+        return ["Computer wins! Rock beats scissors!", "c"]
     }else if (c === "paper" && p === "rock"){
-        console.log("Computer wins! Paper beats rock!")
+        return ["Computer wins! Paper beats rock!", "c"]
     }else if (c === "scissors" && p === "paper"){
-        console.log("Computer wins! Scissors beats paper!")
+        return ["Computer wins! Scissors beats paper!", "c"]
     }
 }
-
+function game() {
+    
+    for (let i = 0; i < 5; i++){
+        winner = determineWinner();
+        console.log (winner[0])
+        if (winner[1] == "p") {
+            playerScore ++
+        }else if (winner[1] == "c") {
+            computerScore ++
+        }
+    }
+    if (computerScore > playerScore) {
+        console.log(`Computer wins with a score of ${computerScore}`)
+    }else if (playerScore > computerScore) {
+        console.log(`Player wins with a score of ${playerScore}`)
+    }else {
+        console.log("Tie")
+    }
+}
 // call determine winner function
-determineWinner(playerSelection, computerSelection);
-
-// if user picks sizzors and computer picks rock
-    // computer wins
-// if user picks rock and computer picks paper
-    // computer wins
-// if user picks paper and computer picks sizzors
-    // computer wins
-
-// if player selection is the same
-    // tie
-
-//output the winner in the form of a console log statement
-
-//count point acumulation
-
-// play a 5 match game 
+game();
